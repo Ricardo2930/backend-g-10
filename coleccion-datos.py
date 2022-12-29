a98a82365ba1b8053024b6c7d2065add4fcad913
Ricardo2930 <rhuaytan@gmail.com>
2022-12-28 23:05:37 -0500

    postman

---
+++ coleccion-datos.py
@@ -0,0 +1,18 @@
+#Arreglos (listas) -- Se pueden modificar (agregar, eliminar elementos) 
+nombre = ['Edu','Joel','Richard','Mike','Rick']
+otros = [1,'Agosto',False,[1,3,4],10.5]
+
+#Tuplas -- Solo que no se pueden modificar (inmutables)
+notas = (1,10,20,13,14,'Eduardo')
+
+#Diccionarios --- es ordenado por su llaves, no por sus posiciones
+usuario = {
+    'nombre':'Eduardo',
+    'apellido':'De Rivero',
+    'edad':30,
+    'soltero':True
+}
+
+#Conjuntos (SET) -- coleccion de datos desordenada, que si se pueden modificar, sirve para ver si un elemento esta o no en ese conjunto
+datos = {10.80,'juan','arequipa'}
+print (datos[0]) 
--- dia-1/inicios-flask.py
+++ dia-1/inicios-flask.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 #si el archivo es eñ archivo principal del proyecto su valor de la variable __name__ sera '__main__' caso contrario sera none (vacio)
 app = Flask (__name__)
 #una sola instancia de clase 
 #patron de diseño de software (singleton) 
+
+#creamos la variable USUARIOS =[]
+usuarios = [
+    {
+        'nombre':'Eduardo',
+        'apellido':'Juarez'
+    },
+    {
+        'nombre':'Juana',
+        'apellido':'Rodriguez'
+    },
+    {
+        'nombre':'Roberto ',
+        'apellido':'Castillo'
+    }
+]
 
 #un decorador se usa con el @ y sirve para modificar cierto metodo de una clase sin la necesidad de modificar el funcionamiento natural (es una modificacion parcial) luego de utilizar el decorador se crea una funcion que sera la nueva funcionalidad de ese metodo 
 @app.route('/')
@@ -10,16 +26,46 @@ app = Flask (__name__)
 def inicio ():
     return 'Hola desde mi servidor de Flask'
 
+#END POINT (punto final) declaración de la finalizacion de la url que indicara que accion se debe realizar
 @app.route('/mostrar-hora', methods=['GET','POST'])
 
 def mostrarHora ():
+    #CONTROLLER (controlaor) es una funcionalidad que se realiza dentro de un determinado endpoint
+    #request -- me dara toda la informacion que viene desde el cliente
+    print (request.method)
+    if request.method == 'GET':
+        return {
+            'content':'Me hiciste GET'
+        }
+    elif request.method == 'POST':
+        return {
+            'content':'Me hiciste POST'
+        }
+
     #no se suele retornar strings (cadena de texto) sino que se utiliza diccionario
     return {'content':'22:50:25'}
 
+@app.route ('/usuarios', methods=['GET','POST'])
+def gestionUsuario ():
+    if request.method == 'GET':
+        #devolveremos los usuarios
+        return {
+            'message':'Los usuarios son',
+            'content': usuarios
+        }
+    elif request.method == 'POST':
+        #agregar un nuevo usuario
+        #request.data -- mostrara la info proveniente del body en formato bytes
+        #request.get_json() > convierte el body entrante en un diccionario desde un JSON
+        print(request.get_json())
+        data = (request.get_json())
+        #con append se agrega data a nuestro arreglo Usuarios
+        usuarios.append(data)
+        return {
+            'message':'usuario agregado exitosamente'
+        }
 
-
-
-#
+# debug = cada vez que modifiquemos algun archivo del proyecto y guardamos, se reiniciara el servidor
 app.run (debug=True)
 
 
