c3086acbe4f204a0c9656b3d5148b168738b01e3
Ricardo2930 <rhuaytan@gmail.com>
2023-01-19 20:21:33 -0500

    search and update en postman

--- semana06/app.py
+++ semana06/app.py
@@ -25,7 +25,22 @@ def productos ():
 @app.route("/productos/crear", methods=['POST']) # ruta para crear productos
 def productosCrear():
     controller = ProductosController()
-    return controller.create(request.json)
+    return controller.crearProducto(request.json) #() es la data
+
+@app.route("/productos/eliminar/<int:producto_id>", methods=['DELETE']) # ruta para ELIMINAR productos
+def productosEliminar(producto_id):
+    controller = ProductosController()
+    return controller.eliminarProducto(producto_id)
+
+@app.route("/productos/actualizar/<int:producto_id>", methods=['PUT'])
+def productosActualizar(producto_id):
+    controller = ProductosController()
+    return controller.actualizarProducto(producto_id, request.json)
+
+@app.route("/productos/buscar/<float:precio>", methods=['GET'])
+def productosBuscar(precio):
+    controller = ProductosController()
+    return controller.buscarProductos(precio)
 
 if __name__ == '__main__':
     app.run (debug=True) # Para que corra la app
