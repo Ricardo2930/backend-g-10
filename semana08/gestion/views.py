5a02425c99f49f8037fd8b87388709059cb4d9c1
Ricardo2930 <rhuaytan@gmail.com>
2023-02-03 20:32:03 -0500

    create class PlatoApiView

--- semana08/gestion/views.py
+++ semana08/gestion/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListCreateAPIView
-from .models import CategoriaModel
-from .serializers import CategoriaSerializer
+from .models import CategoriaModel, PlatoModel
+from .serializers import CategoriaSerializer, PlatoSerializer
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 #List - Listar (get)
 #Create - Crear (post)
 class CategoriaApiView(ListCreateAPIView):
@@ -19,3 +22,19 @@ class CategoriaApiView(ListCreateAPIView):
 
     # def post(self):
     #     pass
+
+class PlatoApiView (ListCreateAPIView):
+    queryset = PlatoModel.objects.all()
+    serializer_class = PlatoSerializer
+
+    #Definimos el def GET para no sobreusar los generics, para poner la logica que queramos
+    def get (self,request:Request): #variable de la clase :Request
+        # Al colocar ':' indicamos que el tipo de dato que sera esa variable en el caso de que no la hemos seteado correctamente
+        #request -> Toda la informacion que viene del cliente
+
+        #SELECT*FROM platos WHERE disponibilidad = True
+        resultado = PlatoModel.objects.filter(disponibilidad=True).all()
+        print(resultado)
+        return Response (data = {
+            'message':'Me hizo get'
+        })
