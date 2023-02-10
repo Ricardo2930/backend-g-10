1cbadbc85f283afb7acd89e71cf2eeaca6ac4588
Ricardo2930 <rhuaytan@gmail.com>
2023-02-09 23:12:48 -0500

    use prisma un project

--- semana09/dia1/index.js
+++ semana09/dia1/index.js
@@ -1,8 +1,15 @@
 //Importamos la libreria EXPRESS
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 // Se va a copia toda la funcionalidad de la liberia express en la varibale servidor
 const servidor = express();
+
+//Ahora mi sercidor podra convertir la info entrante para los JSON
+//MIDDLEWARE para convertir la info entrante a un formato legible
+servidor.use(express.json())
 
 // Creamos nuestra primera ruta
     //req -> es la info que me envia el cliente
